7b19749821f29871da34c4a27af9cf5a25587cbd
qussaif10 <qussaif100+1@gmail.com>
2024-06-15 09:50:55 +0300

    Added noice neovim plugin and removed lazy-lock.json from the repo

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -14,6 +14,10 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 
+		dependencies = {
+			"hrsh7th/cmp-cmdline",
+		},
+
 		config = function()
 			local cmp = require("cmp")
 
@@ -44,6 +48,27 @@ return {
 					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
 				}),
 			})
 		end,
